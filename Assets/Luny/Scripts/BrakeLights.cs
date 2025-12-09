af10237740904c14c3bafae0d6abe3a574dcdbca
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-09 17:01:19 +0100

    add more lights

---
+++ Assets/Luny/Scripts/BrakeLights.cs
@@ -0,0 +1,22 @@
+﻿using LunyScratch;
+using System;
+using static LunyScratch.Blocks;
+
+public sealed class BrakeLights : ScratchBehaviour
+{
+	protected override void OnScratchReady()
+	{
+		base.OnScratchReady();
+
+		const String Brakelights = "BrakeLights";
+
+		Run(Disable(Brakelights));
+
+		// blinking signal lights
+		RepeatForever(
+			If(IsKeyPressed(Key.S),
+					Enable(Brakelights))
+				.Else(Disable(Brakelights))
+		);
+	}
+}
