5a050192198202af0e85dc8f42d6ab43e58c5d96
CodeSmile-PC\CodeSmile <42713966+CodeSmile-0000011110110111@users.noreply.github.com>
2025-12-19 20:08:58 +0100

    update test script

--- Assets/ExampleLunyScript.cs
+++ Assets/ExampleLunyScript.cs
@@ -1,15 +1,48 @@
 using Luny;
+using System;
 
 /// <summary>
-/// Example LunyScript that demonstrates the basic structure.
+/// Example LunyScript demonstrating the block system.
 /// This script will automatically bind to any GameObject named "ExampleLunyScript" in the scene.
 /// </summary>
-public class ExampleLunyScript : LunyScript.LunyScript
+internal sealed class ExampleLunyScript : LunyScript.LunyScript
 {
-	public override void OnStartup() =>
-		// TODO: In future phases, users will construct sequences here:
-		// When.KeyPressed("Space").Do(() => Audio.Play("Jump"));
-		// When.Collision().With("Enemy").Do(() => Variables["Health"] = 0);
-		// For now, this is just a placeholder demonstrating the clean API
-		LunyLogger.LogInfo($"====> {nameof(ExampleLunyScript)} runs OnStartup !!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+	public override void Build()
+	{
+		// Set up variables
+		Variables["Health"] = 100;
+		Variables["Name"] = "Player1";
+		GlobalVariables["GameScore"] = 0;
+
+		// Log messages on Update
+		OnUpdate(Log("ExampleLunyScript Update tick"));
+
+		// Multi-block sequence on Update
+		OnUpdate(
+			Log("Multi-block sequence start"),
+			Do(() =>
+			{
+				var health = Variables.Get<Int32>("Health");
+				Variables["Health"] = health - 1;
+				LunyLogger.LogInfo($"Health decreased to {health - 1}", this);
+
+				// I might consider putting the context back, but then we'd need two kinds of contexts ...
+				//Log("TEST TEST TEST").Execute(context);
+			}),
+			Log("Multi-block sequence end")
+		);
+
+		// Fixed step logging
+		OnFixedStep(Log("ExampleLunyScript FixedStep tick"));
+
+		// Late update logging
+		OnLateUpdate(Log("ExampleLunyScript LateUpdate tick"));
+
+		// Demonstrate global variables
+		OnUpdate(Do(() =>
+		{
+			var score = GlobalVariables.Get<Int32>("GameScore");
+			GlobalVariables["GameScore"] = score + 1;
+		}));
+	}
 }
